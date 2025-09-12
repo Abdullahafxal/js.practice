54cf4e9d8187e5c91bd8cd66c813829f69b119c2
Abdullahafxal <abdullahaafzal980@gmail.com>
2025-09-12 19:38:28 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,11 @@
-console.log("Hello, World From Node!");
-
+const express = require('express');
+const app = express();
 const port = 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
