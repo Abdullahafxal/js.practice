0e66fc0a58de5fbc550a0c47af177fd52f555636
Abdullahafxal <abdullahaafzal980@gmail.com>
2025-09-15 09:14:46 +0000

    Resolve conflict in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world, from EC2 — powered by Node.js!');
+    res.end('Hello world, from EC2 Instance — powered by Node.js!');
 });
 
 server.listen(PORT, () => {
