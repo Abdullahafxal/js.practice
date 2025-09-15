1937483e5783b66683073cd7b9fc6f445f7d5972
Abdullahafxal <abdullahaafzal980@gmail.com>
2025-09-15 14:46:37 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world,from EC2!');
+    res.end('Hello world,from EC2! Instance');
 });
 
 server.listen(PORT, () => {
