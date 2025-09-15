d4aa412cbcbd3b1acc54e12a179d2104dc29e4e3
Abdullahafxal <abdullahaafzal980@gmail.com>
2025-09-15 07:40:03 +0000

    update server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world  from EC2!');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
